3c5fd0abf031ce5f80a8b6e6eeb3840dc7e7e276
riedeljosh <josh.riedel98@gmail.com>
2022-07-18 18:17:18 -0500

    removing deprecated apps

--- truss/truss/settings.py
+++ truss/truss/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'PlanetDisplay.apps.PlanetdisplayConfig',
-    'django_mysql',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

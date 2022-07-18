da7e3d28fc656c7d9da4239917a0515b1ce59b57
riedeljosh <josh.riedel98@gmail.com>
2022-07-18 16:54:46 -0500

    getting info from api

--- truss/PlanetDisplay/views.py
+++ truss/PlanetDisplay/views.py
@@ -2,14 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Planet
+import requests, json
 
 # Create your views here.
 def index(request):
     # Curl
-    url = 'https://swapi.dev/api/planets/'
-
-    p = Planet(climate = "yes", diameter = 1, gravity = "yes", name = "yes", orbital_period = 1, population = 1, residents = ["mike", "billy"], rotation_period = 1, surface_water = 1, terrain = "yes", url = "yes")
-    planet_list = [p]
+    url = 'https://swapi.dev/api/planets/?format=json'
+    r = requests.get(url).json()['results']
+    planet_list = []
+    for result in r:
+        planet_list.append(
+                Planet(
+                        name=result['name'], 
+                        diameter=result['diameter'], 
+                        gravity=result['gravity'], 
+                        climate=result['climate'], 
+                        orbital_period=result['orbital_period'], 
+                        population=result['population'], 
+                        residents=result['residents'],
+                        rotation_period=result['rotation_period'], 
+                        surface_water=result['surface_water'], 
+                        terrain=result['terrain'], 
+                        url=result['url']
+                    )
+            )
+    
     template = loader.get_template('PlanetDisplay/index.html')
     context = {
         'planets': planet_list,
